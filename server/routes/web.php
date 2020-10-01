b5b5f1315139263fb1a3b62e54073a2bdcf04503
takumi_kurogi <takumines1@gmail.com>
2020-10-01 10:24:30 +0900

    [update]ルーティング変更

--- server/routes/web.php
+++ server/routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('auth.register_form');
+Route::post('/register', 'Auth\RegisterController@register')->name('auth.register');
