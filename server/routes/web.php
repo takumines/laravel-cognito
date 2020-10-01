f84990ca2c652e713afb8ffaf4e1083cb72cb8a2
takumi_kurogi <takumines1@gmail.com>
2020-10-01 10:38:19 +0900

    [update]laravel/ui install

--- server/routes/web.php
+++ server/routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 
 Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('auth.register_form');
 Route::post('/register', 'Auth\RegisterController@register')->name('auth.register');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
