caa73ab6aaa6d2d4f332c7f9660fd9ee5efaa83b
takumi_kurogi <takumines1@gmail.com>
2020-10-01 19:25:30 +0900

    [update]cognitoでのログイン機能を追加

--- server/app/Http/Controllers/Auth/LoginController.php
+++ server/app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Aws\CognitoIdentityProvider\Exception\CognitoIdentityProviderException;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -37,4 +42,69 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * ログイン処理を行う
+     *
+     * @param Request $request
+     * @return Response|\Symfony\Component\HttpFoundation\Response|void
+     * @throws ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if ($this->hasTooManyLoginAttempts($request)) {
+
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        try {
+            if ($this->attemptLogin($request)) {
+                return $this->sendLoginResponse($request);
+            }
+
+        } catch (CognitoIdentityProviderException $ce) {
+            return $this->sendFailedCognitoResponse($ce);
+
+        } catch (\Exception $e) {
+            return $this->sendFailedLoginResponse($request);
+        }
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    /**
+     * @param CognitoIdentityProviderException $exception
+     * @throws ValidationException
+     */
+    private function sendFailedCognitoResponse(CognitoIdentityProviderException $exception)
+    {
+        throw ValidationException::withMessages([
+            $this->username() => $exception->getAwsErrorMessage()
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @throws ValidationException
+     */
+    protected function sendLockoutResponse(Request $request)
+    {
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+
+        throw ValidationException::withMessages([
+            $this->username() => [
+                Lang::get('auth.throttle', [
+                    'seconds' => $seconds,
+                    'minutes' => ceil($seconds / 60),
+                ])
+            ],
+        ])->status(Response::HTTP_TOO_MANY_REQUESTS);
+    }
+
 }
