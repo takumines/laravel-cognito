fca946ace274f5a37b9c41bc174f8501dca1e8f1
takumi_kurogi <takumines1@gmail.com>
2020-10-06 19:32:52 +0900

    [update]apiルーティング修正

--- server/routes/api.php
+++ server/routes/api.php
@@ -12,6 +12,13 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::namespace('Api')->group(function () {
+    Route::post('register', 'Auth\RegisterController@register');
+    Route::post('login', 'Auth\LoginController@login');
+});
 
-Route::post('register', 'Api\Auth\RegisterController@register');
-Route::post('login', 'Api\Auth\LoginController@login');
+Route::namespace('Api')->middleware('auth:api')->group(function() {
+    Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify');
+    Route::post('email/resend', 'Auth\VerificationController@resend');
+    Route::get('/home', 'ApiController@index');
+});
