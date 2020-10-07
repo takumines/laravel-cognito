94b40ffb609b02da5f63deca3025a3164af88e70
takumi_kurogi <takumines1@gmail.com>
2020-10-07 14:00:30 +0900

    [update]ルーティング修正

--- server/routes/api.php
+++ server/routes/api.php
@@ -15,9 +15,11 @@ use Illuminate\Http\Request;
 Route::namespace('Api')->group(function () {
     Route::post('register', 'Auth\RegisterController@register');
     Route::post('login', 'Auth\LoginController@login');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+    Route::post('password/reset/{token}', 'Auth\ResetPasswordController@reset');
 });
 
-Route::namespace('Api')->middleware('auth:api')->group(function() {
+Route::namespace('Api')->middleware('auth:api')->group(function () {
     Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify');
     Route::post('email/resend', 'Auth\VerificationController@resend');
     Route::get('/home', 'ApiController@index');
