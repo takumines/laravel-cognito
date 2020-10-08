e865a9bf094141dfaa089373798535d6b6b6ac86
takumi_kurogi <takumines1@gmail.com>
2020-10-08 10:29:37 +0900

    [update]ルーティング修正

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,6 +19,15 @@ Route::namespace('Api')->group(function () {
 
 Route::namespace('Api')->middleware('auth:api')->group(function () {
     Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify');
-    Route::post('email/resend', 'Auth\VerificationController@resend');
-    Route::get('/home', 'ApiController@index');
+
+    Route::middleware('verified')->group(function () {
+        Route::post('email/resend', 'Auth\VerificationController@resend');
+        Route::get('/', 'ApiController@index');
+        Route::get('/users/{user}', 'ApiController@show');
+
+        Route::namespace('Admin')->middleware('admin')->group(function () {
+            Route::get('admin', 'HomeController@index');
+            Route::get('admin/users/{user}', 'UserController@show');
+        });
+    });
 });
