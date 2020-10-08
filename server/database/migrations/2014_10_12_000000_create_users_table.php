2709108ed931dd43dea74b542096353e46e0a2e5
takumi_kurogi <takumines1@gmail.com>
2020-10-08 10:25:58 +0900

    [update]roleカラムを追加

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('cognito_username')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('role')->default(Role::MEMBER);
             $table->rememberToken();
             $table->timestamps();
         });
