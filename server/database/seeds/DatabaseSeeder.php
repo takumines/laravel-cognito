a98bf93e737af456726bde4ae36d46793c77ee0f
takumi_kurogi <takumines1@gmail.com>
2020-10-14 10:33:52 +0900

    [updat]seederを追加

--- server/database/seeds/DatabaseSeeder.php
+++ server/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(FacilitySeeder::class);
+        $this->call(MembershipTypeSeeder::class);
+        $this->call(FacilityMembershipTypeSeeder::class);
     }
 }
