9b07f80797c83a741ca39dd37428c282f2c7ba31
takumi_kurogi <takumines1@gmail.com>
2020-10-14 10:35:43 +0900

    [updat]migrationの追加

---
+++ server/database/migrations/2020_10_13_015547_create_facilities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFacilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->string('description', 1000);
+            $table->string('image', 191);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facilities');
+    }
+}
