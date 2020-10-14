8e502375e7b2af650a2c1a6e866eb07b5892ade6
takumi_kurogi <takumines1@gmail.com>
2020-10-14 10:38:20 +0900

    [add]enumの追加

---
+++ server/app/Enums/MembershipType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+use MyCLabs\Enum\Enum;
+
+class MembershipType extends Enum
+{
+    public const RESIDENCE_RESORT = 'residence_resort';
+
+    public const RESIDENCE = 'residence';
+
+    public const COWORKING_FIX = 'coworking_fix';
+
+    public const COWORKING_FREE = 'coworking_free';
+}
