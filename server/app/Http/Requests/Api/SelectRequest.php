d174c6b198ed184794d000613ce9579af9fddce9
takumi_kurogi <takumines1@gmail.com>
2020-10-15 11:21:03 +0900

    [add]申請時のフォームリクエスト追加

---
+++ server/app/Http/Requests/Api/SelectRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SelectRequest extends ApiRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'property_id'        => ['required', 'integer', 'exists:App\Models\Property,id'],
+            'membership_type_id' => ['required', 'integer', 'exists:App\Models\MembershipType,id'],
+        ];
+    }
+}
