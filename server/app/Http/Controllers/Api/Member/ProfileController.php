2ca61d17af41db9dffb9602e06bd72dd7f196a95
takumi_kurogi <takumines1@gmail.com>
2020-10-15 20:01:12 +0900

    [update]本人確認書類のアップロード処理追加

--- server/app/Http/Controllers/Api/Member/ProfileController.php
+++ server/app/Http/Controllers/Api/Member/ProfileController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers\Api\Member;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\IdentificationUploadImage;
 use App\Http\Requests\Api\ProfileRequest;
 use App\Http\Resources\Member\User;
 use App\Models\Profile;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class ProfileController extends Controller
 {
-    public function update(ProfileRequest $request, Profile $profile)
+    /**
+     * 基本情報・エントリーシート登録・更新
+     *
+     * @param ProfileRequest $request
+     * @param Profile $profile
+     * @return User
+     */
+    public function update(ProfileRequest $request)
     {
         $user = $request->user();
         if ($user->profile === null) {
@@ -19,8 +28,29 @@ class ProfileController extends Controller
         } else {
             $user->profile()->update($request->all());
         }
-//        $profile->insert($request);
 
         return new User($user);
+    }
+
+    /**
+     * 本人確認書類のアップロード
+     *
+     * @param IdentificationUploadImage $request
+     * @param Filesystem $filesystem
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function identificationUpload(IdentificationUploadImage $request, Filesystem $filesystem)
+    {
+        $user = $request->user();
+        $userId = $user->id;
+
+        $extension = $request['identification_image']->getClientOriginalExtension();
+        $filename = $filesystem->hash($request['identification_image']) . '.' . $extension;
+        // 画像の向きを補正
+        $resize_img = Image::make($request['identification_image'])->orientate()->save();
+        $path = 'upload/identification/user/' . $userId . '/' . $filename;
+        Storage::put($path, (string)$resize_img);
+
+        return response()->json($path);
     }
 }
