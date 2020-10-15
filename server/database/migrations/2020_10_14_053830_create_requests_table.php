53516194495bfd30ed4c320c7b97063a4620f244
takumi_kurogi <takumines1@gmail.com>
2020-10-15 11:19:03 +0900

    [update]requestテーブルの再構築

---
+++ server/database/migrations/2020_10_14_053830_create_requests_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Enums\RequestStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('property_id')->unsigned();
+            $table->bigInteger('membership_type_id')->unsigned();
+            $table->string('status','50')->default(RequestStatus::DRAUGHT);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('property_id')->references('id')->on('properties')->onDelete('cascade');
+            $table->foreign('membership_type_id')->references('id')->on('membership_types')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
