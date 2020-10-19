bc881e2495ac7789d000bf78410d4df99ab1ba8b
takumi_kurogi <takumines1@gmail.com>
2020-10-19 14:24:13 +0900

    [update]ルーティング修正

--- server/routes/api.php
+++ server/routes/api.php
@@ -15,12 +15,11 @@ Route::namespace('Api')->group(function () {
     Route::post('login', 'Auth\LoginController@login');
     Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
     Route::post('password/reset/{token}', 'Auth\ResetPasswordController@reset');
+    Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify');
+    Route::post('email/resend', 'Auth\VerificationController@resend');
 });
 
 Route::namespace('Api')->middleware('auth:api')->group(function () {
-    Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify');
-    Route::post('email/resend', 'Auth\VerificationController@resend');
-
     Route::middleware('verified')->group(function () {
         Route::get('/', 'ApiController@index');
         Route::get('users/{user}', 'ApiController@show');
