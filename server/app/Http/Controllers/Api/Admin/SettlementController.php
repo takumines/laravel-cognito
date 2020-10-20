86ba6562da74c1c549ee94191a04cb7c05dc909a
takumi_kurogi <takumines1@gmail.com>
2020-10-20 17:51:04 +0900

    [update]管理者側の未キャプチャ決済処理の修正

--- server/app/Http/Controllers/Api/Admin/SettlementController.php
+++ server/app/Http/Controllers/Api/Admin/SettlementController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Api\Admin;
 
+use App\Enums\ApplicationStatus;
 use App\Http\Controllers\Controller;
+use App\Models\Application;
 use Illuminate\Http\Request;
 use Stripe\StripeClient;
 
@@ -15,10 +17,13 @@ class SettlementController extends Controller
      * @return \Illuminate\Http\JsonResponse
      * @throws \Stripe\Exception\ApiErrorException
      */
-    public function capture(Request $request)
+    public function capture(Request $request, Application $application)
     {
+        $user = $request->user();
         $stripe = new StripeClient(config('services.stripe.secret'));
         $stripe->paymentIntents->capture($request->charge_id);
+        $currentApplication = $application->where('user_id', $user->id)->firstOrFail();
+        $currentApplication->status = ApplicationStatus::APPROVED;
 
         return response()->json([
             'message' => 'success'
