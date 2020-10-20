c68034f3029e6f41f807a46139bb2521b04a432a
takumi_kurogi <takumines1@gmail.com>
2020-10-20 17:44:57 +0900

    [update]不要な改行を削除

--- server/app/Http/Controllers/Api/Admin/HomeController.php
+++ server/app/Http/Controllers/Api/Admin/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
--- server/app/Http/Requests/Api/RegisterRequest.php
+++ server/app/Http/Requests/Api/RegisterRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests\Api;
 
-
 class RegisterRequest extends ApiRequest
 {
     /**
